227bc899293a9583c16316581d4bc5b9886e9d71
medineannamammedova <medineannamammedova@Medine-MacBook-Pro.fritz.box>
2021-12-21 12:11:21 +0100

    ilk

---
+++ src/QaAnswers/C06.java
@@ -0,0 +1,10 @@
+package QaAnswers;
+
+public class C06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/QaAnswers/C07.java
@@ -0,0 +1,10 @@
+package QaAnswers;
+
+public class C07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
